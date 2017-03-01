50579f4f1e7150bb40c4014c95a3bf7a80f23d73
twalatka <twalatka@gmail.com>
2017-02-28 17:53:00 -0700

    JS Function basics - Johnny Five Feb 28, 2017

---
+++ src/index.js
@@ -0,0 +1,9 @@
+export const johnnyFive = {
+    greet(name) {
+        if (!name) return 'Johnny Five is Alive';
+
+        return `Hello, ${name}. I am Johnny 5.  I am alive!`;
+    }
+}
+
+export const shout = message => `${message.toUpperCase()}!!!`;
